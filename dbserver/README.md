3e9d16b8b011c8b4cc3a4f4ed8a4e53639a2d862
aadevera <aadevera@up.edu.ph>
2018-04-30 22:13:07 +0800

    restructured directories

--- dbserver/README.md
+++
@@ -1,6 +0,0 @@
-# DB Server/ Backend
-
-### Setting up:
-1. `npm install` 
-   * handle errors using `sudo`
-2. `npm start`
--- dbserver/models/index.js
+++
@@ -1,8 +0,0 @@
-// add all models here
-
-require('./user-model');
-require('./class-model');
-require('./post-model');
-require('./comment-model');
-require('./message-model');
-require('./conversation-model');
