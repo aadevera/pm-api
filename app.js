e77fdb87cf63fcd0fc4646f8de18859df3bbfa3b
aadevera <aadevera@up.edu.ph>
2018-05-03 12:12:18 +0800

    Update 3/5/2018, now works alongside mongo

--- app.js
+++ app.js
@@ -12,35 +12,36 @@ const DBNAME = 'Server'
 // mongoose connect
 mongoose.connect(DBURI, { dbName: DBNAME } )
     .then(
-        function () { console.log('Database Connected ...') }, 
-        function(err) { console.log(err) }
+        function ()     { console.log ('Database Connected ...') }, 
+        function(err)   { console.log (err) }
     );
 
 require('./lib/models/index');
-const indexRouter = require('./lib/routes/index');
-const classRouter = require('./lib/routes/class');
-const commentRouter = require('./lib/routes/comment');
-const conversationRouter = require('./lib/routes/conversation');
-const messageRouter = require('./lib/routes/message');
-const postRouter = require('./lib/routes/post');
-const userRouter = require('./lib/routes/user');
+const IndexRouter = require('./lib/routes/index');
+const ClassRouter = require('./lib/routes/class');
+const CommentRouter = require('./lib/routes/comment');
+const ConversationRouter = require('./lib/routes/conversation');
+const MessageRouter = require('./lib/routes/message');
+const PostRouter = require('./lib/routes/post');
+const UserRouter = require('./lib/routes/user');
 
 const app = express();
 //app.use(logger('dev'));
 //app.use(cookieParser());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/class', classRouter);
-app.use('/comment', commentRouter);
-app.use('/conversation', conversationRouter);
-app.use('/message', messageRouter);
-app.use('/post', postRouter);
-app.use('/user', userRouter);
-
+app.use('/', IndexRouter);
+app.use('/class', ClassRouter);
+app.use('/comment', CommentRouter);
+app.use('/conversation', ConversationRouter);
+app.use('/message', MessageRouter);
+app.use('/post', PostRouter);
+app.use('/user', UserRouter);
+app.use("*", (req, res) => {
+    res.status(404).send('Error 404. Page not Found');
+});
 module.exports = app;
