abf22f81e929b4cf68323b15ef64f04a505dff66
aadevera <aadevera@up.edu.ph>
2018-05-03 22:15:13 +0800

    Refactored for easier code comprehension

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-// var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 // var logger = require('morgan');
 
 // setup mongoose connection
@@ -17,30 +16,23 @@ mongoose.connect(DBURI, { dbName: DBNAME } )
     );
 
 require('./lib/models/index');
-const IndexRouter = require('./lib/routes/index');
-const ClassRouter = require('./lib/routes/class');
-const CommentRouter = require('./lib/routes/comment');
-const ConversationRouter = require('./lib/routes/conversation');
-const MessageRouter = require('./lib/routes/message');
-const PostRouter = require('./lib/routes/post');
-const UserRouter = require('./lib/routes/user');
-
+const Routers = require('./lib/routes/index');
 const app = express();
 //app.use(logger('dev'));
-//app.use(cookieParser());
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', IndexRouter);
-app.use('/class', ClassRouter);
-app.use('/comment', CommentRouter);
-app.use('/conversation', ConversationRouter);
-app.use('/message', MessageRouter);
-app.use('/post', PostRouter);
-app.use('/user', UserRouter);
+app.use('/', Routers.IndexRouter);
+app.use('/class', Routers.ClassRouter);
+app.use('/comment', Routers.CommentRouter);
+app.use('/conversation', Routers.ConversationRouter);
+app.use('/message', Routers.MessageRouter);
+app.use('/post', Routers.PostRouter);
+app.use('/user', Routers.UserRouter);
 app.use("*", (req, res) => {
     res.status(404).send('Error 404. Page not Found');
 });
